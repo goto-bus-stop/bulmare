c1184c940dec70ea71c8b057fc63aa9c2d0cf0b2
René Kooi <renee@kooi.me>
2017-01-11 14:48:02 +0100

    Add <Menu> wrapper component.

--- src/index.js
+++ src/index.js
@@ -116,6 +116,7 @@ export const CardFooterItem = bulmaComponent('span', 'card-footer-item')
 export const CardFooterLink = bulmaComponent('a', 'card-footer-item')
 
 // http://bulma.io/documentation/components/menu/
+export const Menu = bulmaComponent('aside', 'menu')
 export const MenuLabel = bulmaComponent('p', 'menu-label')
 export const MenuList = bulmaComponent('ul', 'menu-list')
 export const MenuItem = 'li'
