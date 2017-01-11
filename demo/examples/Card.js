9e44a2df2eaef08997733ff7a187da59bfc0d04c
René Kooi <renee@kooi.me>
2017-01-11 14:47:26 +0100

    Add Card example.

---
+++ demo/examples/Card.js
@@ -0,0 +1,19 @@
+import { Card, CardHeader, CardContent, CardFooter, Content, Icon } from 'bulmare'
+
+export default () => (
+  <div>
+    <Card>
+      <CardHeader
+        title="Component"
+        icon={<Icon name="angle-down" />}
+      />
+      <CardContent>
+        <Content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+          iaculis mauris. <a>@bulmaio</a>. <a>#css</a> <a>#responsive</a><br />
+          <small>11:09 PM - 1 Jan 2016</small>
+        </Content>
+      </CardContent>
+    </Card>
+  </div>
+)
--- demo/pages/index.js
+++ demo/pages/index.js
@@ -1,11 +1,15 @@
 import { Section } from 'bulmare'
 
+import Card from '../examples/Card'
 import Menu from '../examples/Menu'
 import Message from '../examples/Message'
 import Tag from '../examples/Tag'
 
 export default () => (
   <div>
+    <Section>
+      <Card />
+    </Section>
     <Section>
       <Menu />
     </Section>
