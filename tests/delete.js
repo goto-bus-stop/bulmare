ae6a5ade6f17a832fd0fbd9c467b79a77a3841c2
René Kooi <renee@kooi.me>
2017-01-16 19:52:08 +0100

    Add tests for Delete button.

---
+++ tests/delete.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import test from 'ava'
+import { shallow } from 'enzyme'
+import { Delete } from '../src'
+
+test('Delete renders a button with class "delete"', (t) => {
+  t.true(shallow(<Delete />).dive().equals(
+    <button className="delete" />
+  ))
+})
+
+test('Deletes support sizes', (t) => {
+  t.true(shallow(<Delete small />).hasClass('is-small'))
+  t.true(shallow(<Delete medium />).hasClass('is-medium'))
+  t.true(shallow(<Delete large />).hasClass('is-large'))
+})
