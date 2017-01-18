0300c3742b3ff0285b9dda0a9a820e6475d88144
René Kooi <renee@kooi.me>
2017-01-18 11:26:52 +0100

    Add Icon tests

---
+++ tests/icon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import test from 'ava'
+import { shallow } from 'enzyme'
+import { Icon } from '../src'
+
+test('Icon renders an span with class "icon"', (t) => {
+  t.true(shallow(<Icon />).dive().dive().is('span.icon'))
+})
+
+test('Icon "name" prop is mapped to a font-awesome className', (t) => {
+  t.true(shallow(<Icon name="check" />).childAt(0).dive().dive().equals(
+    <i className="fa fa-check" />
+  ))
+})
