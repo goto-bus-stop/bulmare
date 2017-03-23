0b2bdab25d2944653b99fd175331ed2c3f322268
René Kooi <renee@kooi.me>
2017-03-23 16:18:46 +0100

    Support `size={12}` style grid size modifiers in Tiles.

--- src/grid.js
+++ src/grid.js
@@ -1,19 +1,23 @@
 import cx from 'classnames'
+import compose from 'recompose/compose'
 import mapProps from 'recompose/mapProps'
 import { bulmaComponent, withModifiers } from './internal'
 
+const withGridSize = mapProps(({ size, className, ...props }) => ({
+  ...props,
+  className: cx(size && `is-${size}`, className)
+}))
+
 // http://bulma.io/documentation/grid/columns/
 export const Columns = bulmaComponent('div', 'columns')
-export const Column = bulmaComponent('div', 'column',
-  mapProps(({ size, className, ...props }) => ({
-    ...props,
-    className: cx(size && `is-${size}`, className)
-  }))
-)
+export const Column = bulmaComponent('div', 'column', withGridSize)
 
-export const Tile = bulmaComponent('div', 'tile', withModifiers({
-  ancestor: 'is-ancestor',
-  parent: 'is-parent',
-  child: 'is-child',
-  vertical: 'is-vertical'
-}))
+export const Tile = bulmaComponent('div', 'tile', compose(
+  withModifiers({
+    ancestor: 'is-ancestor',
+    parent: 'is-parent',
+    child: 'is-child',
+    vertical: 'is-vertical'
+  }),
+  withGridSize
+))
